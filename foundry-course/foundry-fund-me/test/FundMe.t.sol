e0eb34cdbf088490e695dbb0497612485e3d0d9e
d4rm5 <dantemartinez06@gmail.com>
2025-04-25 19:11:52 -0300

    updated changes

--- foundry-course/foundry-fund-me/test/FundMe.t.sol
+++ foundry-course/foundry-fund-me/test/FundMe.t.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        // fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public view {
@@ -18,7 +21,7 @@ contract FundMeTest is Test {
 
     function testOwnerIsMsgSender() public view {
         console.log(fundMe.i_owner());
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
     // us --> FundMeTest --> FundMe
 
